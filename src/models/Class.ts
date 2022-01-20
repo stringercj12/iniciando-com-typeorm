002b8e6bd2910a365c6848a781cb4f265465c50c
Jefferson Ferreira <jefferson14489@gmail.com>
2022-01-20 18:35:40 -0300

    Model de class

---
+++ src/models/Class.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Class {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    length: 100,
+    unique: true
+  })
+  name: string;
+
+  @Column()
+  duration: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+}
